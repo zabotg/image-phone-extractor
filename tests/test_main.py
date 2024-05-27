b8c2c19b92d411bb3ce423fbaf6d78ac47c395e3
Guilherme Zabot <zabot.gui@gmail.com>
2024-05-27 11:51:37 -0300

    Fix tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import pytest
-from main import get_image_paths, save_phone_numbers_to_file
+from utils import get_image_paths, save_phone_numbers_to_file
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
